321b903e9ae0dc156a68525741608850db2d96f3
Vladimir <troyanskii1998@yandex.ru>
2023-10-19 00:31:49 +0300

    Оформление страницы 404

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -1,6 +1,27 @@
+import { Link } from 'react-router-dom';
+import Footer from '../../componets/footer/footer';
+import Header from '../../componets/header/header';
+import { AppRoute } from '../../config';
+
+
 function Page404(): JSX.Element {
   return (
-    <p>Page Not Found</p>
+    <div className="wrapper">
+      <Header />
+      <main>
+        <div className="page-content">
+          <section className="catalog">
+            <div className="container">
+              <h1 className="title title--h2">Страница не существует</h1>
+              <div className="page-content__columns">
+                <Link className="btn btn--purple product-card__btn" to={AppRoute.Catalog}>Вернуться на главную страницу</Link>
+              </div>
+            </div>
+          </section>
+        </div>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
