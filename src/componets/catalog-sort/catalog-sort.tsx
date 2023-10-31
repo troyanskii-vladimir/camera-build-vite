df6e2db9630f15b82c25d10a7875af7930b65f9e
Vladimir <troyanskii1998@yandex.ru>
2023-11-01 02:49:42 +0300

    Настраивает специальные маршруты для показа сортировки

--- src/componets/catalog-sort/catalog-sort.tsx
+++ src/componets/catalog-sort/catalog-sort.tsx
@@ -1,4 +1,85 @@
+import { useEffect, useState } from 'react';
+import browserHistory from '../../browser-history';
+import { AppRoute } from '../../config';
+import { useLocation } from 'react-router-dom';
+
+enum SortType {
+  Price = 'price',
+  Marks = 'marks',
+}
+
+enum SortOrder {
+  ToTop = 'totop',
+  ToLow = 'tolow',
+}
+
 function CatalogSort(): JSX.Element {
+  const [sortParams, setSortParams] = useState({
+    type: SortType.Price,
+    order: SortOrder.ToTop,
+  });
+
+  const location = useLocation();
+  const extraUrl = location.search;
+
+  const [extraUrlWithoutSort, setExtraUrlWithoutSort] = useState(extraUrl);
+
+  useEffect(() => {
+    setSortParams({
+      type: SortType.Price,
+      order: SortOrder.ToTop,
+    });
+
+    if (extraUrl.match(/sort/i)) {
+      const sortNumberElement = Number(extraUrl.match(/sort/i)?.index) + 5;
+
+      const sortType = extraUrl.slice(sortNumberElement, sortNumberElement + 5) as SortType;
+      const sortOrder = extraUrl.slice(sortNumberElement + 5, sortNumberElement + 10) as SortOrder;
+
+      setExtraUrlWithoutSort(extraUrl.replace(sortType, '').replace(sortOrder, '').replace('?sort=', ''));
+
+      setSortParams({
+        type: sortType,
+        order: sortOrder,
+      });
+    }
+
+  }, [extraUrl]);
+
+
+  const onSetPriceButtonClick = () => {
+    setSortParams({
+      type: SortType.Price,
+      order: sortParams.order,
+    });
+    browserHistory.push(`${AppRoute.Catalog}?sort=${SortType.Price}${sortParams.order}${extraUrlWithoutSort}`);
+  };
+
+  const onSetMarksButtonClick = () => {
+    setSortParams({
+      type: SortType.Marks,
+      order: sortParams.order,
+    });
+    browserHistory.push(`${AppRoute.Catalog}?sort=${SortType.Marks}${sortParams.order}${extraUrlWithoutSort}`);
+  };
+
+  const onSetToTopButtonClick = () => {
+    setSortParams({
+      type: sortParams.type,
+      order: SortOrder.ToTop,
+    });
+    browserHistory.push(`${AppRoute.Catalog}?sort=${sortParams.type}${SortOrder.ToTop}${extraUrlWithoutSort}`);
+  };
+
+  const onSetToLowButtonClick = () => {
+    setSortParams({
+      type: sortParams.type,
+      order: SortOrder.ToLow,
+    });
+    browserHistory.push(`${AppRoute.Catalog}?sort=${sortParams.type}${SortOrder.ToLow}${extraUrlWithoutSort}`);
+  };
+
+
   return (
     <div className="catalog-sort">
       <form action="#">
@@ -10,12 +91,19 @@ function CatalogSort(): JSX.Element {
                 type="radio"
                 id="sortPrice"
                 name="sort"
-                defaultChecked
+                checked={sortParams.type === SortType.Price}
+                onChange={onSetPriceButtonClick}
               />
               <label htmlFor="sortPrice">по цене</label>
             </div>
             <div className="catalog-sort__btn-text">
-              <input type="radio" id="sortPopular" name="sort" />
+              <input
+                type="radio"
+                id="sortPopular"
+                name="sort"
+                checked={sortParams.type === SortType.Marks}
+                onChange={onSetMarksButtonClick}
+              />
               <label htmlFor="sortPopular">по популярности</label>
             </div>
           </div>
@@ -25,8 +113,9 @@ function CatalogSort(): JSX.Element {
                 type="radio"
                 id="up"
                 name="sort-icon"
-                defaultChecked
+                checked={sortParams.order === SortOrder.ToTop}
                 aria-label="По возрастанию"
+                onChange={onSetToTopButtonClick}
               />
               <label htmlFor="up">
                 <svg width={16} height={14} aria-hidden="true">
@@ -40,6 +129,8 @@ function CatalogSort(): JSX.Element {
                 id="down"
                 name="sort-icon"
                 aria-label="По убыванию"
+                checked={sortParams.order === SortOrder.ToLow}
+                onChange={onSetToLowButtonClick}
               />
               <label htmlFor="down">
                 <svg width={16} height={14} aria-hidden="true">
